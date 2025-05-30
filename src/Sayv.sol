28c0558dd245b35d87f6dd638b9753a321eb2702
X-O1 <series-x-01@protonmail.com>
2025-05-30 00:29:35 -0700

    Updated

--- src/Sayv.sol
+++ src/Sayv.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.30;
 import {ITokenRegistry} from "../src/interfaces/ITokenRegistry.sol";
 
 contract Sayv {
-    ITokenRegistry public iTokenRegistry;
+    ITokenRegistry internal iTokenRegistry;
 
     error NOT_OWNER(address caller, address owner);
 
@@ -17,9 +17,8 @@ contract Sayv {
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
             revert NOT_OWNER(msg.sender, i_owner);
-        } else {
-            _;
         }
+        _;
     }
 
     function setTokenRegistry(address _tokenRegistryAddress) external onlyOwner {
