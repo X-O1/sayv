5d3fa189a8ae4cccc1e526719445d69cfef9a252
X-O1 <series-x-01@protonmail.com>
2025-06-07 19:41:14 -0700

    Updated

--- src/SayvVaultFactory.sol
+++ src/SayvVaultFactory.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "./Errors.sol";
+import "./SayvErrors.sol";
 import {SayvVault} from "./SayvVault.sol";
 
 contract SayvVaultFactory {
     address immutable i_owner;
-    address[] public s_allActiveVaults; // To start USDC and aUSDC
+    address[] public s_allActiveVaults;
 
     mapping(address vault => bool isActive) public s_activeVaults;
 
-    event Vault_Created(address indexed token, address indexed activeYieldPool);
+    event Vault_Created(address indexed token, address indexed activePool);
 
     constructor() {
         i_owner = msg.sender;
@@ -23,15 +23,15 @@ contract SayvVaultFactory {
         _;
     }
 
-    function createVault(address _token, address activeYieldPool) external onlyOwner returns (SayvVault) {
+    function createVault(address _token, address activePool) external onlyOwner returns (SayvVault) {
         if (s_activeVaults[_token]) {
             revert VAULT_ALREADY_EXIST();
         }
         s_activeVaults[_token] = true;
         s_allActiveVaults.push(_token);
 
-        SayvVault sayvVault = new SayvVault(_token, activeYieldPool);
-        emit Vault_Created(_token, activeYieldPool);
+        SayvVault sayvVault = new SayvVault(_token, activePool);
+        emit Vault_Created(_token, activePool);
         return sayvVault;
     }
 }
