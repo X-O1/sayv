20ad0e2c2c0e3c40c8eeb0f924c21668074c79ae
X-O1 <series-X-01@protonmail.com>
2025-06-15 23:10:54 -0700

    Updated

--- test/unit/YieldAdapterTest.t.sol
+++ test/unit/YieldAdapterTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 import {Test, console} from "forge-std/Test.sol";
 import {YieldAdapter} from "../../src/YieldAdapter.sol";
 import {IERC20} from "@openzeppelin/ERC20/IERC20.sol";
+import {IPool} from "@aave-v3-core/interfaces/IPool.sol";
 
 /**
  * @title Test for YieldAdapter.sol on the BASE Mainnet
@@ -12,7 +13,8 @@ import {IERC20} from "@openzeppelin/ERC20/IERC20.sol";
 contract YieldAdaptertTest is Test {
     YieldAdapter yieldAdapter;
     address yieldAdapterAddress;
-    address dev = 0xa3EC4bE8cAdBf2257be5bc149de628177b75b276;
+    address dev = 0x7Cc00Dc8B6c0aC2200b989367E30D91B7C7F5F43;
+    address fakeUser = 0x7e6Af92Df2aEcD6113325c0b58F821ab1dCe37F6;
     address usdcAddress = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
     address aUSDC = 0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB;
     address addressProvider = 0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D;
@@ -31,10 +33,19 @@ contract YieldAdaptertTest is Test {
 
             vm.prank(dev);
             IERC20(usdcAddress).approve(yieldAdapterAddress, type(uint256).max);
-            vm.prank(user);
+
+            vm.prank(dev);
+            IERC20(aUSDC).approve(yieldAdapterAddress, type(uint256).max);
+            vm.prank(dev);
+            IERC20(usdcAddress).transfer(fakeUser, 100);
+
+            vm.prank(fakeUser);
+            IERC20(aUSDC).approve(yieldAdapterAddress, type(uint256).max);
+            vm.prank(fakeUser);
             IERC20(usdcAddress).approve(yieldAdapterAddress, type(uint256).max);
-            vm.prank(user2);
-            IERC20(usdcAddress).approve(yieldAdapterAddress, type(uint256).max);
+
+            vm.prank(yieldAdapterAddress);
+            IERC20(aUSDC).approve(poolAddress, type(uint256).max);
         }
     }
 
@@ -45,40 +56,37 @@ contract YieldAdaptertTest is Test {
     }
 
     modifier deposit() {
-        vm.prank(dev);
-        yieldAdapter.depositToVault(usdcAddress, 1000);
-        vm.prank(user);
-        yieldAdapter.depositToVault(usdcAddress, 5000);
-        vm.prank(user2);
-        yieldAdapter.depositToVault(usdcAddress, 4000);
+        vm.prank(fakeUser);
+        yieldAdapter.depositToVault(100);
         _;
     }
 
     function testDepositAndAccounting() public ifBaseMainnet {
-        vm.prank(dev);
-        yieldAdapter.depositToVault(usdcAddress, 1000);
-        vm.prank(user);
-        yieldAdapter.depositToVault(usdcAddress, 750);
-        vm.prank(user2);
-        yieldAdapter.depositToVault(usdcAddress, 500);
-
-        assertEq(yieldAdapter._getAccountDeposits(dev, usdcAddress), 1000);
-        assertEq(yieldAdapter._getAccountDeposits(user, usdcAddress), 750);
-        assertEq(yieldAdapter._getAccountDeposits(user2, usdcAddress), 500);
-        assertEq(yieldAdapter._getVaultYieldTokenBalance(), 2250);
+        vm.prank(fakeUser);
+        yieldAdapter.depositToVault(100);
+        assertEq(IERC20(usdcAddress).balanceOf(fakeUser), 0);
+        assertEq(IERC20(aUSDC).balanceOf(yieldAdapterAddress), 100);
+        assertEq(yieldAdapter.getAccountShareValue(fakeUser), 100);
     }
 
-    function testWithdrawAndAccounting() public ifBaseMainnet deposit {
-        // test withdrawing available funds and
-        vm.prank(dev);
-        yieldAdapter.withdrawFromVault(usdcAddress, 500);
-        assertEq(yieldAdapter._getAccountDeposits(dev, usdcAddress), 500);
-        assertEq(yieldAdapter._getVaultYieldTokenBalance(), 9500);
+    function testWithdrawAndAccounting() public ifBaseMainnet {
+        vm.prank(fakeUser);
+        yieldAdapter.depositToVault(100);
 
-        // test withrawing more % of the pool than i own
-        // test withdrawing more than available equity
+        vm.warp(block.timestamp + 356 days);
+
         vm.prank(dev);
-        vm.expectRevert();
-        yieldAdapter.withdrawFromVault(usdcAddress, 1500);
+        yieldAdapter.depositToVault(100);
+
+        vm.prank(fakeUser);
+        yieldAdapter.withdrawFromVault(100);
+        console.logUint(yieldAdapter.getAccountShareValue(fakeUser));
+
+        // assertEq(IERC20(usdcAddress).balanceOf(fakeUser), 100);
+        // assertEq(yieldAdapter.getAccountShareValue(fakeUser), 0);
+        // assertEq(IERC20(aUSDC).balanceOf(yieldAdapterAddress), 0);
+        // assertEq(IERC20(usdcAddress).balanceOf(fakeUser), 100);
+
+        // IPool(poolAddress).supply(usdcAddress, 1, msg.sender, 0);
     }
 }
